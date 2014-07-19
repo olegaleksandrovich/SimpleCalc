c2caa5103dd9774152ba05b88763736a894fa8dd
oleg <olegpetrushov@me.com>
2014-07-19 09:25:02 +0300

    First commit

---
+++ SimpleCalcTests/SimpleCalcTests.m
@@ -0,0 +1,39 @@
+//
+//  SimpleCalcTests.m
+//  SimpleCalcTests
+//
+//  Created by Oleg Petrushov on 7/7/14.
+//  Copyright (c) 2014 Oleg Petrushov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleCalcTests : XCTestCase
+
+@end
+
+@implementation SimpleCalcTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
